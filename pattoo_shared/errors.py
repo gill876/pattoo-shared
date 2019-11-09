bfad83118395a4753aa91e0601a81bc2e60b93a0
Peter Harrison <blackriver@simiya.com>
2019-11-08 20:14:39 -0800

    Test

--- pattoo_shared/errors.py
+++ pattoo_shared/errors.py
@@ -42,7 +42,7 @@ Minimum value {} is greater than {}. Please fix.\
         'log2die_safe(', 'log2warning(',
         'log2debug(', 'log2live(', 'log2warn(', 'log2die(', 'log2quiet(',
         'log2info(', 'log2screen(', 'log2see(', r'.modify(', r'.query(',
-        r'.replace(', '.add_all(')
+        r'.add(.*?,%d+)', r'.replace(', '.add_all(')
     error_codes = []
     available_codes = []
     entries = 5
